f8f0068a5f0f47853aac8dad8d27c67a867f3a27
Tabean19 <Tabean19@gmail.com>
2021-03-19 22:33:51 -0500

    updated fetch for prod

--- src/context/AppState.js
+++ src/context/AppState.js
@@ -18,12 +18,23 @@ const AppState = (props) => {
 
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
-  //get apps
+  //get apps -- test
   const getApps = () => {
     setLoading();
     const res = require('../data.json');
     dispatch({ type: GET_APPS, payload: res });
   };
+  // //get apps -- prod
+  // const getApps = async () => {
+  //   setLoading();
+  //   const options = {
+  //     method: 'GET',
+  //     headers: { 'Content-Type': 'application/json' },
+  //   };
+  //   const res = await fetch('api/getApps', options);
+  //   const data = await res.json();
+  //   dispatch({ type: GET_APPS, payload: data });
+  // };
   //set active application
   const setActiveApp = (app) => {
     dispatch({ type: SET_ACTIVE, payload: app });
